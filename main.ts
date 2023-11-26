d286d404758f49ab70b3324870b3bbd55e444b6d
MikaelLMortensen <MikaelLMortensen@users.noreply.github.com>
2023-11-26 15:44:20 +0100

    Opdater main.ts

--- main.ts
+++ main.ts
@@ -1,28 +1,63 @@
 
+
+function GetDateTimeFromCallback(callbackString : string)
+{
+    let dateObject = { year: 1900, month: 1, day: 1, hour: 0, minute: 0, second: 0, daylightsaving: false, errorCode : 0 }
+    dateObject.errorCode = 1;
+    if (callbackString == null || callbackString == '')
+        return dateObject;
+
+    dateObject.errorCode = 2;
+    let parts = callbackString.split(',');
+    if (parts.length < 2)
+        return dateObject;
+
+    dateObject.errorCode = 3;
+    let currentDateTimePart = parts[1];
+    parts = currentDateTimePart.split('":"');
+    if (parts.length != 2)
+        return dateObject;
+    currentDateTimePart = parts[1].replaceAll('"', '');
+
+    dateObject.errorCode = 4;
+    parts = currentDateTimePart.split('T');
+    if (parts.length != 2)
+        return dateObject;
+    let date = parts[0];
+    let time = parts[1];
+
+    dateObject.errorCode = 5;
+    parts = time.split('+');
+    if (parts.length != 2)
+        return dateObject;
+    time = parts[0];
+
+
+    dateObject.errorCode = 6;
+    parts = date.split('-');
+    if (parts.length != 3)
+        return dateObject;
+
+    dateObject.year = parseInt(parts[0]);
+    dateObject.month = parseInt(parts[1]);
+    dateObject.day = parseInt(parts[2]);
+
+    dateObject.errorCode = 7;
+    parts = time.split(':')
+    if (parts.length != 2)
+        return dateObject;
+    dateObject.hour = parseInt(parts[0]);
+    dateObject.minute = parseInt(parts[1]);
+
+     return dateObject;
+}
+
 let callbackStr = 'mycallback({"$id":"1","currentDateTime":"2023-11-25T14:15+01:00","utcOffset":"01:00:00","isDayLightSavingsTime":false,"dayOfTheWeek":"Saturday","timeZoneName":"Central Europe Standard Time","currentFileTime":133453953101897145,"ordinalDate":"2023-329","serviceResponse":null});';
+let dateObject = GetDateTimeFromCallback(callbackStr);
 
-let parts = callbackStr.split(',');
-let currentDateTimePart = parts[1];
-parts = currentDateTimePart.split('":"');
-currentDateTimePart = parts[1].replaceAll('"', '');
-
-parts = currentDateTimePart.split('T');
-let date = parts[0];
-let time = parts[1];
-parts = time.split('+');
-time = parts[0];
-parts = date.split('-');
-
-let year = parseInt(parts[0]);
-let month = parseInt(parts[1]);
-let day = parseInt(parts[2]);
-
-parts = time.split(':')
-let hour = parseInt(parts[0]);
-let minute = parseInt(parts[1]);
-
-timeanddate.setDate(month, day, year);
-timeanddate.set24HourTime(hour,minute,0);
-
-basic.showString(timeanddate.dateTime());
-
+if (dateObject.errorCode == 0)
+    basic.showString('Error: ' + dateObject.errorCode);
+else {
+    basic.showString('D: ' + dateObject.year + '-' + dateObject.month + '-' + dateObject.day);
+    basic.showString('T: ' + dateObject.hour + ':' + dateObject.minute);
+}
